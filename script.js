afa710df6b6f661f0c0aaa8e2aacccaab9e3ff6f
Matheus de Oliveira <eumatheusdeoliveirajc@gmail.com>
2023-12-09 19:32:30 -0300

    fsdf

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-function showMenu(act) {
-    var menu = document.querySelector("#menu-mobile");
-    if(act == 1) {menu.style.display = 'block'}
-    if(act == 2) {menu.style.display = 'none'}
-}
+const menu = document.querySelector('.menu-mobile');
+const line1 = document.querySelector('.line1');
+const line2 = document.querySelector('.line2');
+const line3 = document.querySelector('.line3');
+const header = document.getElementsByTagName('header')[0];
+const menuIcon = document.querySelector('.menu-mobile-icon').addEventListener('click', function(){
+    if(menu.style.display == 'block'){
+        header.style.borderBottom = 'none';
+        menu.style.display = 'none';
+        menu.classList.remove('menu-active');
+        line1.classList.remove('line1active');
+        line2.classList.remove('line2active');
+        line3.classList.remove('line3active');
+    }else{
+        header.style.borderBottom = '2px solid rgba(255, 255, 255, 0.5)';
+        menu.style.display = 'block';
+        menu.classList.add('menu-active');
+        line1.classList.add('line1active');
+        line2.classList.add('line2active');
+        line3.classList.add('line3active');
+    }
+})
